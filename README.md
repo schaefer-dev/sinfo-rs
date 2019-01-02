47cee1200fe1118afd0cc1de923a00b9fc751bd8
Daniel Schäfer <schaefer.dev@gmail.com>
2019-01-02 16:06:48 +0100

    printing username, os, env, hostname

---
+++ README.md
@@ -0,0 +1,1 @@
+sinfo-rs is a tool that outputs information about your system in a visually pleasing way written entirely in Rust.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+extern crate whoami;
+extern crate colored;
+
+
+use std::fmt;
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    let system = System::read_data();
+
+    println!("{}", system);
+
+}
+
+struct System {
+    username: String,
+    os: String,
+    env: String,
+    hostname: String,
+}
+
+impl System {
+
+    fn read_data() -> System {
+        System {
+            username: whoami::username(),
+            os: whoami::os(),
+            env: whoami::env().to_string(),
+            hostname: whoami::hostname(),
+        }
+    }
+}
+
+impl fmt::Display for System {
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let username_prefix = "Username:".cyan();
+        let hostname_prefix = "Hostname:".cyan();
+        let os_prefix = "OS:".cyan();
+        let env_prefix = "ENV:".cyan();
+
+        let write_result = write!(f, "{} {}\n", username_prefix, self.username);
+        match write_result {
+            Ok(_v) => (),
+            Err(_e) => return write_result,
+        }
+
+        let write_result = write!(f, "{} {}\n", hostname_prefix, self.hostname);
+        match write_result {
+            Ok(_v) => (),
+            Err(_e) => return write_result,
+        }
+
+        let write_result = write!(f, "{} {}\n", os_prefix, self.os);
+        match write_result {
+            Ok(_v) => (),
+            Err(_e) => return write_result,
+        }
+
+        let write_result = write!(f, "{} {}\n", env_prefix, self.env);
+        match write_result {
+            Ok(_v) => (),
+            Err(_e) => return write_result,
+        }
+
+        Ok(())
+    }
 }
