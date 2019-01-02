2490180af2f14658b8309c3b065901984cda4bb2
Daniel Schäfer <schaefer.dev@gmail.com>
2019-01-02 17:23:23 +0100

    implemented tests for local substring removal

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 extern crate whoami;
 extern crate colored;
 
+mod string_utils;
+use crate::string_utils::*;
 
 use std::fmt;
 use colored::*;
-use std::collections::VecDeque as VecDeque;
 
 fn main() {
     let mut system = System::read_data();
     system.post_processing();
 
     println!("{}", system);
-
-    let mut test_input: String = "hackintosh.local.test.local".to_string();
-    remove_local_substrings(&mut test_input);
-    println!("Output :'{}'", test_input);
-
 }
 
 struct System {
@@ -76,46 +72,4 @@ impl fmt::Display for System {
 
         Ok(())
     }
-}
-
-// Remove '.local' from Hostname
-fn remove_local_substrings(input_string: &mut String) {
-        // Build Vector out of string split around . character
-        let hostname_raw = input_string.clone();
-        let hostname_split = hostname_raw.split(".");
-        let mut hostname_vec: Vec<&str> = hostname_split.collect();
-
-        let mut remove_indexes: VecDeque<usize> = VecDeque::new();
-
-        let mut iter_index: usize = 0;
-        for element in &hostname_vec {
-            if *element == "local" {
-                remove_indexes.push_back(iter_index);
-                println!("DEBUG: Hostname Index {} has to be removed later", iter_index);
-            }
-            iter_index += 1;
-        }
-
-        // Remove all the indexes that contains strings that are removed
-        let mut remove_counter: usize = 0;
-        while remove_indexes.is_empty() == false {
-            let remove_index = remove_indexes.pop_front().unwrap();
-            println!("DEBUG: Removing Index {} now", remove_index);
-            hostname_vec.remove(remove_index - remove_counter);
-            remove_counter += 1;
-        }
-
-        // if no elements remain, write Error
-        if hostname_vec.is_empty() {
-            *input_string = "ERROR".to_string();
-            return;
-        }
-
-        // Rebuilding Hostname String from remaining vector elements
-        *input_string = hostname_vec.pop().unwrap().to_string();
-
-        for host_part in &hostname_vec {
-            input_string.push_str(".");
-            input_string.push_str(host_part);
-        }
 }
---
+++ src/string_utils.rs
@@ -0,0 +1,90 @@
+use std::collections::VecDeque as VecDeque;
+
+// Remove '.local' from Hostname
+pub fn remove_local_substrings(input_string: &mut String) {
+        // Build Vector out of string split around . character
+        let hostname_raw = input_string.clone();
+        let hostname_split = hostname_raw.split(".");
+        let mut hostname_vec: VecDeque<&str> = hostname_split.collect();
+
+        let mut remove_indexes: VecDeque<usize> = VecDeque::new();
+
+        let mut iter_index: usize = 0;
+        for element in &hostname_vec {
+            if *element == "local" {
+                remove_indexes.push_back(iter_index);
+                println!("DEBUG: Hostname Index {} has to be removed later", iter_index);
+            }
+            iter_index += 1;
+        }
+
+        // Remove all the indexes that contains strings that are removed
+        let mut remove_counter: usize = 0;
+        while remove_indexes.is_empty() == false {
+            let remove_index = remove_indexes.pop_front().unwrap();
+            println!("DEBUG: Removing Index {} now", remove_index);
+            hostname_vec.remove(remove_index - remove_counter);
+            remove_counter += 1;
+        }
+
+        // if no elements remain, write Error
+        if hostname_vec.is_empty() {
+            *input_string = "ERROR".to_string();
+            return;
+        }
+
+        // Rebuilding Hostname String from remaining vector elements
+        *input_string = hostname_vec.pop_front().unwrap().to_string();
+
+        for host_part in &hostname_vec {
+            input_string.push_str(".");
+            input_string.push_str(host_part);
+        }
+}
+
+#[test]
+pub fn test_remove_local_substrings() {
+
+    let mut test_input_1: String = "hackintosh.local.test.local".to_string();
+    let test_output_1: String = "hackintosh.test".to_string();
+    remove_local_substrings(&mut test_input_1);
+    assert_eq!(test_input_1, test_output_1);
+
+    let mut test_input_2: String = "local.hackintosh.local.test.local".to_string();
+    let test_output_2: String = "hackintosh.test".to_string();
+    remove_local_substrings(&mut test_input_2);
+    assert_eq!(test_input_2, test_output_2);
+
+    let mut test_input_3: String = "hackintosh.local.local".to_string();
+    let test_output_3: String = "hackintosh".to_string();
+    remove_local_substrings(&mut test_input_3);
+    assert_eq!(test_input_3, test_output_3);
+
+    let mut test_input_4: String = "hackintosh".to_string();
+    let test_output_4: String = "hackintosh".to_string();
+    remove_local_substrings(&mut test_input_4);
+    assert_eq!(test_input_4, test_output_4);
+
+    let mut test_input_5: String = "local".to_string();
+    let test_output_5: String = "ERROR".to_string();
+    remove_local_substrings(&mut test_input_5);
+    assert_eq!(test_input_5, test_output_5);
+}
+
+#[test]
+pub fn test_remove_local_substrings_none() {
+
+    let mut test_input_4: String = "hackintosh".to_string();
+    let test_output_4: String = "hackintosh".to_string();
+    remove_local_substrings(&mut test_input_4);
+    assert_eq!(test_input_4, test_output_4);
+}
+
+#[test]
+pub fn test_remove_local_substrings_error() {
+
+    let mut test_input_5: String = "local".to_string();
+    let test_output_5: String = "ERROR".to_string();
+    remove_local_substrings(&mut test_input_5);
+    assert_eq!(test_input_5, test_output_5);
+}
