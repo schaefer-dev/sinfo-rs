48037f964569a65a0ed92d55ae0ff49ab0f4144b
Daniel Schäfer <schaefer.dev@gmail.com>
2019-01-02 18:19:47 +0100

    Graphics card is now being recognized on osx

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::fmt;
 use colored::*;
 
 use std::path::Path;
-use std::ffi::OsStr;
+use std::process::Command;
 
 fn main() {
     let mut system = System::read_data();
@@ -23,6 +23,8 @@ struct System {
     os: String,
     env: String,
     hostname: String,
+    graphics: String,
+    is_mac: bool,
 }
 
 impl System {
@@ -33,6 +35,8 @@ impl System {
             os: whoami::os(),
             env: whoami::env().to_string(),
             hostname: whoami::hostname(),
+            graphics: "unknown".to_string(),
+            is_mac: false,
         }
     }
 
@@ -45,6 +49,12 @@ impl System {
         // NOTE: whoami library outputs a typo in the case of OS being unknown
         if self.os == "uknown" || self.os == "unknown" {
             self.os = System::get_os();
+            if self.os[0..3] == "OSX".to_string() {
+                self.is_mac = true;
+                self.get_osx_graphics();
+            } else {
+                self.get_linux_graphics();
+            }
         }
     }
 
@@ -55,12 +65,27 @@ impl System {
             return "Arch Linux".to_string();
         }
 
+        // use os_type library to figure out OS type and version
         let os = os_type::current_platform();
-
         let os_string: String = format!("{:?} {}", os.os_type, os.version);
-
         return os_string;
     }
+
+    fn get_osx_graphics(&mut self) {
+        let output = Command::new("sh").arg("-c").arg("echo $(system_profiler SPDisplaysDataType | awk '/Model/{for (i=1; i<=NF-2; i++) $i = $(i+2); NF-=2; print}' | paste -sd '/' -)").output().expect("failed to execute Graphics command");
+        match std::str::from_utf8(&output.stdout) {
+            Ok(v) => self.graphics = v.to_string(),
+            Err(_e) => self.graphics = "ERROR".to_string(),
+        }
+        println!("{}", std::str::from_utf8(&output.stdout).unwrap());
+    }
+
+    fn get_linux_graphics(&mut self) {
+        // TODO: implement in the future
+    }
+
+
+
 }
 
 impl fmt::Display for System {
@@ -70,6 +95,7 @@ impl fmt::Display for System {
         let hostname_prefix = "Hostname:".cyan();
         let os_prefix = "Distro:".cyan();
         let env_prefix = "ENV:".cyan();
+        let graphics_prefix = "GPU:".cyan();
 
         let write_result = write!(f, "{} {}\n", username_prefix, self.username);
         match write_result {
@@ -90,6 +116,12 @@ impl fmt::Display for System {
         }
 
         let write_result = write!(f, "{} {}\n", env_prefix, self.env);
+        match write_result {
+            Ok(_v) => (),
+            Err(_e) => return write_result,
+        }
+
+        let write_result = write!(f, "{} {}\n", graphics_prefix, self.graphics);
         match write_result {
             Ok(_v) => (),
             Err(_e) => return write_result,
