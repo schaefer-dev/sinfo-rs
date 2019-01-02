1d9ad2e255d780663a5cf86cd93e8b42bcca5d22
Daniel Schäfer <schaefer.dev@gmail.com>
2019-01-02 17:14:08 +0100

    implementation of local removal

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,17 @@ extern crate colored;
 
 use std::fmt;
 use colored::*;
+use std::collections::VecDeque as VecDeque;
 
 fn main() {
-    let system = System::read_data();
+    let mut system = System::read_data();
+    system.post_processing();
 
     println!("{}", system);
+
+    let mut test_input: String = "hackintosh.local.test.local".to_string();
+    remove_local_substrings(&mut test_input);
+    println!("Output :'{}'", test_input);
 
 }
 
@@ -28,6 +34,11 @@ impl System {
             env: whoami::env().to_string(),
             hostname: whoami::hostname(),
         }
+    }
+
+    // Perform post-processing to make Strings look more appealing
+    fn post_processing(&mut self) -> () {
+        remove_local_substrings(&mut self.hostname);
     }
 }
 
@@ -65,4 +76,46 @@ impl fmt::Display for System {
 
         Ok(())
     }
+}
+
+// Remove '.local' from Hostname
+fn remove_local_substrings(input_string: &mut String) {
+        // Build Vector out of string split around . character
+        let hostname_raw = input_string.clone();
+        let hostname_split = hostname_raw.split(".");
+        let mut hostname_vec: Vec<&str> = hostname_split.collect();
+
+        let mut remove_indexes: VecDeque<usize> = VecDeque::new();
+
+        let mut iter_index: usize = 0;
+        for element in &hostname_vec {
+            if *element == "local" {
+                remove_indexes.push_back(iter_index);
+                println!("DEBUG: Hostname Index {} has to be removed later", iter_index);
+            }
+            iter_index += 1;
+        }
+
+        // Remove all the indexes that contains strings that are removed
+        let mut remove_counter: usize = 0;
+        while remove_indexes.is_empty() == false {
+            let remove_index = remove_indexes.pop_front().unwrap();
+            println!("DEBUG: Removing Index {} now", remove_index);
+            hostname_vec.remove(remove_index - remove_counter);
+            remove_counter += 1;
+        }
+
+        // if no elements remain, write Error
+        if hostname_vec.is_empty() {
+            *input_string = "ERROR".to_string();
+            return;
+        }
+
+        // Rebuilding Hostname String from remaining vector elements
+        *input_string = hostname_vec.pop().unwrap().to_string();
+
+        for host_part in &hostname_vec {
+            input_string.push_str(".");
+            input_string.push_str(host_part);
+        }
 }
